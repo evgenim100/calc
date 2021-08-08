5c566b04daa42af49f183f2789dfd295dfeb98de
evgenimus <dvh2010#yandex.ru>
2021-08-08 23:10:55 +0300

    Lesson2 added

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-let money;
-let income; 
-let addExpenses; 
-let deposit;
-let mission;
-let period;
-alert('сообщение с любым текстом');
+let money = 100000;
+let income = 'freelance'; 
+let addExpenses = 'интернет, такси, коммуналка'; 
+let deposit = false;
+let mission = 1000000 ;
+let period = 12;
+let budgetDay = money/30;
 console.log('сообщение с любым текстом');
+console.log(typeof money);
+console.log(typeof income);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+console.log(budgetDay);
+
+
